f88bc9d2ede40b30cee80904e503697d2ba07cd8
lauren_brady <laurenbrady01@tamu.edu>
2022-09-20 16:43:49 -0500

    rover deforms terrain

---
+++ Assets/RoverTerrainModifier.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverTerrainModifier : MonoBehaviour
+{
+    public Terrain terrain;
+    TerrainTool terrainTool;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        terrainTool = FindObjectOfType<TerrainTool>();
+        terrainTool._targetTerrain = terrain;
+    }
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.CompareTag("rover"))
+        {
+            terrainTool.RaiseTerrain(collision.transform.position, terrainTool.strength, terrainTool.brushHeight, terrainTool.brushHeight);
+        }
+    }
+}
