9faa6bb5a2bcc63b3e66fb01ef05423dbbc54fe7
Henrynaut <neilmchenry@gmail.com>
2022-09-24 09:33:22 +0700

    Updated movement.cs script

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -12,13 +12,16 @@ public class movement : MonoBehaviour
     {
         if (transform.position != target[current].position) {  
             Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);  
-            GetComponent < Rigidbody > ().MovePosition(pos);  
+            // GetComponent < Rigidbody > ().MovePosition(pos);  
         } else current = (current + 1) % target.Length;       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+            if (transform.position != target[current].position) {  
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);  
+            // GetComponent < Rigidbody > ().MovePosition(pos);  
+        } else current = (current + 1) % target.Length;           
     }
 }
